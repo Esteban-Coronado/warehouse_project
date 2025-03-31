2100132406096be394f7b511e6803274184672e6
Pedro Cruz <pedro.cruz.lopez001@gmail.com>
2025-03-31 15:11:31 -0500

    fix CORS

--- src/main/java/com/warehouse_project/warehouse_project/config/CorsConfig.java
+++ src/main/java/com/warehouse_project/warehouse_project/config/CorsConfig.java
@@ -7,16 +7,16 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class CorsConfig {
-
     @Bean
     public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**") // Permite todas las rutas
-                        .allowedOrigins("http://localhost:3000") // Permite peticiones desde React
+                registry.addMapping("/api/**")
+                        .allowedOriginPatterns("http://localhost:3000", "http://127.0.0.1:3000") 
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
+                        .exposedHeaders("Authorization", "Content-Type") 
                         .allowCredentials(true);
             }
         };
