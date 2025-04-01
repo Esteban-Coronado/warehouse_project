71dfcb20762576bbb0856761ac16c4ef8dcf138e
PedroCruz00 <111622891+PedroCruz00@users.noreply.github.com>
2025-04-01 16:06:00 -0500

    Revert "Pedrodev"

--- src/main/java/com/warehouse_project/warehouse_project/dto/VirtualWarehouseConfigDto.java
+++ src/main/java/com/warehouse_project/warehouse_project/dto/VirtualWarehouseConfigDto.java
@@ -1,9 +1,5 @@
 package com.warehouse_project.warehouse_project.dto;
 
-import lombok.Data;
-
-@Data
 public class VirtualWarehouseConfigDto {
-    private int percentage;
-    private int minDistance;
+    
 }
