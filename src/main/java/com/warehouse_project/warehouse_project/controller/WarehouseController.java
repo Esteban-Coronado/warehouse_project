ef7f4771d9735e16ada4328ee47ed17985ae429c
Pedro Cruz <pedro.cruz.lopez001@gmail.com>
2025-04-01 13:55:30 -0500

    Revert "improve location(latitud longitud)"
    This reverts commit d431aef26b7861ad80592dfd5f6927c7d2fadaa6.

--- src/main/java/com/warehouse_project/warehouse_project/controller/WarehouseController.java
+++ src/main/java/com/warehouse_project/warehouse_project/controller/WarehouseController.java
@@ -36,16 +36,17 @@ public class WarehouseController {
             @ApiResponse(responseCode = "500", description = "Error al crear el almacén")
     })
     @PostMapping
-    public ResponseEntity<Warehouse> createWarehouse(@RequestBody WarehouseCreationDto creationDto) {
-        logger.info("Recibida solicitud para crear nuevo almacén: {}", creationDto.getName());
-        try {
-            Warehouse newWarehouse = warehouseService.createWarehouse(creationDto);
-            return ResponseEntity.ok(newWarehouse);
-        } catch (IOException e) {
-            logger.error("Error al crear almacén", e);
-            return ResponseEntity.internalServerError().build();
-        }
+public ResponseEntity<Warehouse> createWarehouse(@RequestBody WarehouseCreationDto creationDto) {
+    logger.info("Recibida solicitud para crear nuevo almacén: {}", creationDto.getName());
+    try {
+        Warehouse newWarehouse = warehouseService.createWarehouse(creationDto);
+        return ResponseEntity.ok(newWarehouse);
+    } catch (IOException e) {
+        logger.error("Error al crear almacén", e);
+        return ResponseEntity.internalServerError().build();
     }
+}
+
 
     @Operation(summary = "Obtener todos los almacenes", description = "Recupera la lista de todos los almacenes disponibles")
     @ApiResponses(value = {
@@ -72,8 +73,7 @@ public class WarehouseController {
     public ResponseEntity<Warehouse> getWarehouseById(@PathVariable String id) {
         logger.debug("Solicitando almacén con ID: {}", id);
         try {
-            Warehouse warehouse = warehouseService.getWarehouseById(id);
-            return ResponseEntity.ok(warehouse);
+            return ResponseEntity.ok(warehouseService.getWarehouseById(id));
         } catch (Exception e) {
             logger.error("Error al obtener almacén con ID: {}", id, e);
             return ResponseEntity.internalServerError().build();
--- src/main/java/com/warehouse_project/warehouse_project/dto/WarehouseCreationDto.java
+++ src/main/java/com/warehouse_project/warehouse_project/dto/WarehouseCreationDto.java
@@ -1,11 +1,8 @@
 package com.warehouse_project.warehouse_project.dto;
-import com.warehouse_project.warehouse_project.model.Coordinates;
-
 import lombok.Data;
 
 @Data
 public class WarehouseCreationDto {
     private String name;
     private String location;
-    private Coordinates coordinates;
 }
